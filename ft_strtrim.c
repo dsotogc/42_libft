0eb28fb2a15dc464b26d8e70afbed389571724b4
David Soto Garcia <dsoto-ga@c3r14s4.42madrid.com>
2026-06-10 19:53:40 +0200

    feat: add strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: dsoto-ga <dsoto-ga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/10 18:43:17 by dsoto-ga          #+#    #+#             */
-/*   Updated: 2026/06/10 18:59:44 by dsoto-ga         ###   ########.fr       */
+/*   Updated: 2026/06/10 19:49:09 by dsoto-ga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,18 @@
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	
+	size_t	start;
+	size_t	end;
+	char	*str;
+
+	if (!s1 || !set)
+		return (NULL);
+	start = 0;
+	while (s1[start] && ft_strchr(set, s1[start]))
+		start++;
+	end = ft_strlen(s1);
+	while (end > start && ft_strchr(set, s1[end - 1]))
+		end--;
+	str = ft_substr(s1, start, end - start);
+	return (str);
 }
